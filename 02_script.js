ce8298adaf8d138cc9d29204ef37ccc6e01a12e1
junyong <kingjykim@naver.com>
2020-10-07 15:26:15 +0900

    homework2

---
+++ 02_script.js
@@ -0,0 +1,16 @@
+var cam_txt = "2012년 1월에 3박4일로 캄보디아로 패키지투어를 다녀왔다.";
+var jp_txt = "2016년 7월에 4박5일로 7명의 친구들과 일본 도쿄로 여행을 갔다.";
+var hk_txt = "2017년 7월에 3박4일로 3명의 친구들과 홍콩으로 여행을 갔다.";
+var cota_txt = "2019년 10월에 3박4일로 여자친구와 코타키나 발루로 여행을 갔다.";
+function c_Funtion(){
+    document.getElementById("input").innerHTML = cam_txt;
+}
+function j_Funtion(){
+    document.getElementById("input1").innerHTML = jp_txt;
+}
+function h_Funtion(){
+    document.getElementById("input2").innerHTML = hk_txt;
+}
+function co_Funtion(){
+    document.getElementById("input3").innerHTML = cota_txt;
+}
